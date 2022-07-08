d0f86bbda3e0f1d73393762942ad76e0e563867c
Jagadeesh-palanisamy <88223245+Jagadeesh-palanisamy@users.noreply.github.com>
2022-07-08 10:34:11 +0530

    Update bms-monitor.js

--- bms-monitor.js
+++ bms-monitor.js
@@ -1,18 +1,18 @@
 const {expect} = require('chai');
 
 function batteryIsOk(temperature, soc, charge_rate) {
-    if (temperature < 0 || temperature > 45) {
-        console.log('Temperature is out of range!');
-        return false;
-    } else if (soc < 20 || soc > 80) {
-        console.log('State of Charge is out of range!')
-        return false;
-    } else if (charge_rate > 0.8) {
-        console.log('Charge rate is out of range!');
-        return false;
-    }
-    return true;
+  return (checkBattery(temperature, 0, 45, "Temperature") 
+				&& checkBattery(soc, 20, 80, "State of Charge") 
+				&& checkBattery(charge_rate, 0, 0.8, "Charge Rate"));
 }
+
+function checkBattery(toCheck,lowerLimit,upperLimit,checkName) {
+		if (toCheck >= upperLimit || toCheck <= lowerLimit ) {
+			System.out.println(checkName +" is out of range!");
+			return false;
+		}
+		return true;
+	}
 
 expect(batteryIsOk(25, 70, 0.7)).to.be.true;
 expect(batteryIsOk(50, 85, 0)).to.be.false;
