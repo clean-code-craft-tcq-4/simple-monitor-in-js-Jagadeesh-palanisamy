39228e72fa004d3f904cc4efa2e00f1e6e3564b7
Jagadeesh-palanisamy <88223245+Jagadeesh-palanisamy@users.noreply.github.com>
2022-07-13 13:44:21 +0530

    Update bms-monitor.js

--- bms-monitor.js
+++ bms-monitor.js
@@ -1,18 +1,35 @@
-import {Test} from './bms-Test.js';
 const {expect} = require('chai');
-
-function batteryIsOk(temperature, soc, charge_rate) {
-  return (checkBattery(temperature, 0, 45, "Temperature") 
-				&& checkBattery(soc, 20, 80, "State of Charge") 
-				&& checkBattery(charge_rate, 0, 0.8, "Charge Rate"));
+const batteryIsOk = (temperature, soc, chargeRate)=> {
+const tempResult = isTemperatureOK(temperature);
+const socResult =  isSocOK(soc); 
+const chargeResult = isChargeRateOK(chargeRate);
+ return tempResult&&socResult&&chargeResult
 }
 
-function checkBattery(toCheck,lowerLimit,upperLimit,checkName) {
-		if (toCheck >= upperLimit || toCheck <= lowerLimit ) {
-			System.out.println(checkName +" is out of range!");
-			return false;
+const isTemperatureOK = (temperature) =>{
+  return checkBattery(temperature, 0, 45, "Temperature")
+}
+
+const isSocOK = (soc) =>{
+  return checkBattery(soc, 20, 80, "State of Charge")
+}
+
+const isChargeRateOK = (chargeRate) =>{
+  return checkBattery(chargeRate, 0, 0.8, "Charge Rate")
+}
+
+ checkBattery = (toCheck,lowerLimit,upperLimit,statement) => {
+		if (lowerLimit < toCheck <  upperLimit) {
+		printStatement(statement);
+	  return false;
 		}
 		return true;
 	}
-
-Test();
+	
+	const printStatement = (statement) =>{
+	  console.log(statement +" is out of range!")
+	}
+	
+	
+expect(batteryIsOk(25, 70, 0.7)).to.be.true;
+expect(batteryIsOk(50, 85, 0)).to.be.false;
