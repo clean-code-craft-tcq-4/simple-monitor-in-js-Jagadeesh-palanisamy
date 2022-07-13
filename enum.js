0d9620a12a1364eacda64937cc66341c6ecdf18a
Jagadeesh-palanisamy <88223245+Jagadeesh-palanisamy@users.noreply.github.com>
2022-07-13 18:47:06 +0530

    Create enum.js

---
+++ enum.js
@@ -0,0 +1,15 @@
+const limitStatus_en = {
+lowbreach: "LOW_BREACH",
+lowwarning: "LOW_WARNING",
+normal: "NORMAL",
+highwarning: "HIGH_WARNING",
+highbreach: "HIGH_BREACH",
+};
+
+const limitStatus_gm = {
+lowbreach: "VERLETZUNG DES NIEDRIGEN",
+lowwarning: "WARNUNG ZU NIEDRIGEM",
+normal: "NORMAL",
+highwarning: "WARNUNG ZU HOHEM",
+highbreach: "HOHE VERLETZUNG",
+};
