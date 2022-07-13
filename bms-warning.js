493cd34ab366b625e8eec888e6214a4b2e82025c
Jagadeesh-palanisamy <88223245+Jagadeesh-palanisamy@users.noreply.github.com>
2022-07-13 20:36:06 +0530

    Create bms-warning.js

---
+++ bms-warning.js
@@ -0,0 +1,23 @@
+import {limitStatus_en,limitStatus_gm} from './bms-enum.js';
+import {setWarningStatement} from './bms-monitor';
+export const checkWarningLevel = (lowerLimit,upperLimit,value) =>{
+	  const WarningLimit =calculateWarningLimit(upperLimit) ;
+	   if((lowerLimit>value)){
+	     setWarningStatement('lowbreach')
+	  }
+	  if((lowerLimit<=value) && ((lowerLimit+WarningLimit)>=value)){
+	     setWarningStatement('lowwarning')
+	  }
+	   if(((lowerLimit+WarningLimit)<=value)&&((upperLimit-WarningLimit)>=value)){
+	     setWarningStatement('normal')
+	  }
+	  if(((upperLimit-WarningLimit)<=value) && (value>=upperLimit)){
+	   setWarningStatement('highwarning')
+	  }
+	   if((upperLimit>value)){
+	     setWarningStatement('highbreach')
+	  }
+	  }
+ const calculateWarningLimit = (upperLimit) =>{
+	    return Number((upperLimit * 0.05).toFixed(2))  ;
+	  }
