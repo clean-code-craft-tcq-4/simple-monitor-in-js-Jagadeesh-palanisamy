666cdf8752b0e06febbf599eaeb893a70fc3b28a
Jagadeesh-palanisamy <88223245+Jagadeesh-palanisamy@users.noreply.github.com>
2022-07-13 20:50:06 +0530

    Update bms-monitor.js

--- bms-monitor.js
+++ bms-monitor.js
@@ -1,33 +1,78 @@
-import {checkWarningLevel} from './bms-warning.js'
-import {Test} from './bms-test.js'
+const limitStatus_en = {
+lowbreach: "LOW_BREACH",
+lowwarning: "LOW_WARNING",
+normal: "NORMAL",
+highwarning: "HIGH_WARNING",
+highbreach: "HIGH_BREACH",
+};
+
+const limitStatus_gm = {
+lowbreach: "VERLETZUNG DES NIEDRIGEN",
+lowwarning: "WARNUNG ZU NIEDRIGEM",
+normal: "NORMAL",
+highwarning: "WARNUNG ZU HOHEM",
+highbreach: "HOHE VERLETZUNG",
+};
+
 let language = '';
-export const batteryIsOk = (temperature, soc, chargeRate ,lang)=> {
-language = lang;
+
+const batteryIsOk = (temperature, soc, chargeRate ,lang)=> {
+  language = lang;
 const tempResult = isTemperatureOK(temperature);
 const socResult =  isSocOK(soc); 
 const chargeResult = isChargeRateOK(chargeRate);
  return tempResult&&socResult&&chargeResult
 }
+
 const isTemperatureOK = (temperature) =>{
   return checkBattery(temperature, 0, 45, "Temperature")
 }
+
 const isSocOK = (soc) =>{
   return checkBattery(soc, 20, 80, "State of Charge")
   
 }
+
 const isChargeRateOK = (chargeRate) =>{
   return checkBattery(chargeRate, 0, 0.8, "Charge Rate")
 }
- const checkBattery = (toCheck,lowerLimit,upperLimit,statement) => {
-        checkWarningLevel(lowerLimit,upperLimit,toCheck);
+
+ checkBattery = (toCheck,lowerLimit,upperLimit,statement) => {
+ checkWarningLevel(lowerLimit,upperLimit,toCheck);
     if (toCheck >= upperLimit || toCheck <= lowerLimit ) {
 	   	printStatement(statement +" is out of range!");
     return false;
 		}
 		return true;
 	}
- 
-  export const setWarningStatement=(limit)=>{
+	
+	const checkWarningLevel = (lowerLimit,upperLimit,value) =>{
+	  const WarningLimit =calculateWarningLimit(upperLimit) ;
+	  
+	  if((lowerLimit>value)){
+	     setWarningStatement('lowbreach')
+	  }
+	  if((lowerLimit<=value) && ((lowerLimit+WarningLimit)>=value)){
+	     setWarningStatement('lowwarning')
+	  }
+	   if(((lowerLimit+WarningLimit)<=value)&&((upperLimit-WarningLimit)>=value)){
+	     setWarningStatement('normal')
+	  }
+	  if(((upperLimit-WarningLimit)<=value) && (value>=upperLimit)){
+	   setWarningStatement('highwarning')
+	  }
+	   if((upperLimit>value)){
+	     setWarningStatement('highbreach')
+	  }
+	  
+	  
+	}
+	  
+	  const calculateWarningLimit = (upperLimit) =>{
+	    return Number((upperLimit * 0.05).toFixed(2))  ;
+	  }
+	
+   const setWarningStatement=(limit)=>{
        if(language==='english'){
 		     printStatement(limitStatus_en[limit])
 		  }
@@ -35,7 +80,9 @@ const isChargeRateOK = (chargeRate) =>{
 		     printStatement(limitStatus_gm[limit])
 		  }
     }
-	export const printStatement = (statement) =>{
+	
+	
+	
+		const printStatement = (statement) =>{
 	  console.log(statement)
 	}
- Test();
