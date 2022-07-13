217647a3ca00ef586766380564e856982e21319e
Jagadeesh-palanisamy <88223245+Jagadeesh-palanisamy@users.noreply.github.com>
2022-07-13 19:01:02 +0530

    Update warning.js

--- warning.js
+++ warning.js
@@ -19,3 +19,22 @@ const checkWarningLevel = (lowerLimit,upperLimit,value) =>{
 	  
 	  
 	}
+
+	  const calculateWarningLimit = (upperLimit) =>{
+	    return Number((upperLimit * 0.05).toFixed(2))  ;
+	  }
+	
+   const setWarningStatement=(limit)=>{
+       if(language==='english'){
+		     printStatement(limitStatus_en[limit])
+		  }
+		  if(language==='german'){
+		     printStatement(limitStatus_gm[limit])
+		  }
+    }
+	
+	
+	
+		const printStatement = (statement) =>{
+	  console.log(statement)
+	}
