7a6238dd5824902b4c02b1ab8276f27e7edab2e5
Jagadeesh-palanisamy <88223245+Jagadeesh-palanisamy@users.noreply.github.com>
2022-07-13 19:23:12 +0530

    Update bms-warning.js

--- bms-warning.js
+++ bms-warning.js
@@ -1,4 +1,19 @@
-import {limitStatus_en,limitStatus_gm} from './bms-enum.js'
+const limitStatus_en = {
+lowbreach: "LOW_BREACH",
+lowwarning: "LOW_WARNING",
+normal: "NORMAL",
+highwarning: "HIGH_WARNING",
+highbreach: "HIGH_BREACH",
+};
+
+const limitStatus_gm = {
+lowbreach: "VERLETZUNG DES NIEDRIGEN",
+lowwarning: "WARNUNG ZU NIEDRIGEM",
+normal: "NORMAL",
+highwarning: "WARNUNG ZU HOHEM",
+highbreach: "HOHE VERLETZUNG",
+};
+
 const checkWarningLevel = (lowerLimit,upperLimit,value) =>{
 	  const WarningLimit =calculateWarningLimit(upperLimit) ;
 	   if((lowerLimit>value)){
