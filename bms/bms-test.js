cab535d8dcd885ecc4c09e53ea2381fe06567eca
Jagadeesh-palanisamy <88223245+Jagadeesh-palanisamy@users.noreply.github.com>
2022-07-13 20:21:15 +0530

    Update bms-test.js

--- bms/bms-test.js
+++ bms/bms-test.js
@@ -1,4 +1,5 @@
 import {batteryIsOk} from './bms-monitor.js'
+import {expect} from 'chai'
 export const Test=()=>{
 expect(batteryIsOk(25, 70, 0.7,'english')).to.be.true;
 expect(batteryIsOk(50, 85, 0,'german')).to.be.false;
