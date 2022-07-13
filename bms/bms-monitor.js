957d6ce76166a1166ec8512cea2fec7f9d4dc8f8
Jagadeesh-palanisamy <88223245+Jagadeesh-palanisamy@users.noreply.github.com>
2022-07-13 19:36:18 +0530

    Update bms-monitor.js

--- bms/bms-monitor.js
+++ bms/bms-monitor.js
@@ -1,1 +1,28 @@
-
+import {checkWarningLevel,printStatement} from './bms-warning.js'
+const {expect} = require('chai');
+let language = '';
+const batteryIsOk = (temperature, soc, chargeRate ,lang)=> {
+language = lang;
+const tempResult = isTemperatureOK(temperature);
+const socResult =  isSocOK(soc); 
+const chargeResult = isChargeRateOK(chargeRate);
+ return tempResult&&socResult&&chargeResult
+}
+const isTemperatureOK = (temperature) =>{
+  return checkBattery(temperature, 0, 45, "Temperature")
+}
+const isSocOK = (soc) =>{
+  return checkBattery(soc, 20, 80, "State of Charge")
+  
+}
+const isChargeRateOK = (chargeRate) =>{
+  return checkBattery(chargeRate, 0, 0.8, "Charge Rate")
+}
+ const checkBattery = (toCheck,lowerLimit,upperLimit,statement) => {
+        checkWarningLevel(lowerLimit,upperLimit,toCheck);
+    if (toCheck >= upperLimit || toCheck <= lowerLimit ) {
+	   	printStatement(statement +" is out of range!");
+    return false;
+		}
+		return true;
+	}
