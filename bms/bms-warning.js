580b198b1da40df3db471b5c786acc8a2eab2f5d
Jagadeesh-palanisamy <88223245+Jagadeesh-palanisamy@users.noreply.github.com>
2022-07-13 19:37:51 +0530

    Update bms-warning.js

--- bms/bms-warning.js
+++ bms/bms-warning.js
@@ -1,1 +1,34 @@
+import {limitStatus_en,limitStatus_gm} from './bms-enum.js'
+const checkWarningLevel = (lowerLimit,upperLimit,value) =>{
+	  const WarningLimit =calculateWarningLimit(upperLimit) ;
+	   if((lowerLimit>value)){
+	     setWarningStatement('lowbreach')
+	  }
+	  if((lowerLimit<=value) && ((lowerLimit+WarningLimit)>=value)){
+	     setWarningStatement('lowwarning')
+	  }
+	   if(((lowerLimit+WarningLimit)<=value)&&((upperLimit-WarningLimit)>=value)){
+	     setWarningStatement('normal')
+	  }
+	  if(((upperLimit-WarningLimit)<=value) && (value>=upperLimit)){
+	   setWarningStatement('highwarning')
+	  }
+	   if((upperLimit>value)){
+	     setWarningStatement('highbreach')
+	  }
+	  }
+ const calculateWarningLimit = (upperLimit) =>{
+	    return Number((upperLimit * 0.05).toFixed(2))  ;
+	  }
+	 const setWarningStatement=(limit)=>{
+       if(language==='english'){
+		     printStatement(limitStatus_en[limit])
+		  }
+		  if(language==='german'){
+		     printStatement(limitStatus_gm[limit])
+		  }
+    }
+	const printStatement = (statement) =>{
+	  console.log(statement)
+	}
 
